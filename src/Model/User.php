3e1e571c1a424b6ae47a9d033717e1c8e441e8e0
Betu97 <albert.mo@students.salle.url.edu>
2019-07-08 15:25:59 +0200

    Coments

--- src/Model/User.php
+++ src/Model/User.php
@@ -27,7 +27,7 @@ final class User
     /** @var string */
     private $password;
 
-    /** @var string */
+    /** @var bool */
     private $is_active;
 
     /** @var DateTime */
@@ -56,6 +56,7 @@ final class User
      * User constructor.
      * @param string $email
      * @param string $password
+     * @param string $name
      * @param DateTime $createdAt
      * @param DateTime $updatedAt
      */
