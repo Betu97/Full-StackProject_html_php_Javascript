86d67c07b33c3eb5eb603b48bb4a1f61cd066179
Betu97 <albert.mo@students.salle.url.edu>
2019-07-16 13:53:00 +0200

    BuyProduct

--- src/Controller/BuyController.php
+++ src/Controller/BuyController.php
@@ -7,6 +7,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Container\ContainerInterface;
 use SallePW\SlimApp\Model\Item;
+use PHPMailer\PHPMailer\PHPMailer;
 
 final class BuyController
 {
@@ -35,9 +36,21 @@ final class BuyController
         try {
 
             $data = $request->getParsedBody();
-
+            $email = [];
             $repository = $this->container->get('user_repo');
             mail('albert.m_97@hotmail.com', 'An item has been sold!', 'User has bought your product! The phone number of this user is: 651066477');
+
+            $info = $repository->loadItem($data['item']);
+
+            $email ['product'] = $info['title'];
+            if($info['owner'] != -1) {
+                $owner = $repository->loadUser($info['owner']);
+                $email ['email'] = $owner['email'];
+                $buyer = $repository->loadUser($_SESSION['id']);
+                $email ['buyerName'] = $buyer['username'];
+                $email ['buyerPhone'] = $buyer['phone_number'];
+                $this->sendEmail($email);
+            }
             $repository->buy($data['item']);
 
             // We should validate the information before creating the entity
@@ -51,6 +64,43 @@ final class BuyController
             $response->getBody()->write('Unexpected error: ' . $e->getMessage());
             return $response->withStatus(500);
         }
+    }
+
+    public function sendEmail(array $email): Bool
+    {
+        $mail = new PHPMailer();
+
+        //Luego tenemos que iniciar la validación por SMTP:
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true;
+        $mail->Host = "smtp.gmail.com"; // A RELLENAR. Aquí pondremos el SMTP a utilizar. Por ej. mail.midominio.com
+        $mail->Username = "pwpopam@gmail.com"; // A RELLENAR. Email de la cuenta de correo. ej.info@midominio.com La cuenta de correo debe ser creada previamente.
+        $mail->Password = "Asdfasdf1+"; // A RELLENAR. Aqui pondremos la contraseña de la cuenta de correo
+        $mail->Port = 587; // Puerto de conexión al servidor de envio.
+        $mail->From = "pwpopam@gmail.com"; // A RELLENAR Desde donde enviamos (Para mostrar). Puede ser el mismo que el email creado previamente.
+        $mail->FromName = "Pwpop"; //A RELLENAR Nombre a mostrar del remitente.
+        $mail->AddAddress($email['email']); // Esta es la dirección a donde enviamos
+        $mail->IsHTML(true); // El correo se envía como HTML
+        $mail->Subject = "An item has been sold!"; // Este es el titulo del email.
+        $body = $email['buyerName'];
+        $body .= " has bought your product: ";
+        $body .= $email ['product'];
+        $body .="! The phone number of this user is: ";
+        $body .=$email ['buyerPhone'];
+        $mail->Body = $body;
+        try {
+            // Mensaje a enviar.
+            $exito = $mail->Send();
+            // Envía el correo.
+            if ($exito) {
+                return true;
+            } else {
+                return false;
+            }
+        }catch (\Exception $e){
+
+        }
+        return false;
     }
 
 }
