a2cf524536d543d1895ff205207e800091e40251
Betu97 <albert.mo@students.salle.url.edu>
2019-07-18 00:02:55 +0200

    ajax

--- public/assets/js/ajax.js
+++ public/assets/js/ajax.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#login-form').submit(function(event) {
+    $('#login-form').submit(function (event) {
         var payload = {
             username: $('input[name=username]').val(),
             password: $('input[name=password]').val()
@@ -12,18 +12,18 @@ $(document).ready(function() {
             data: JSON.stringify(payload), // our data object
             dataType: 'json' // what type of data do we expect back from the server
         })
-            .done(function(data) {
+            .done(function (data) {
                 console.log(data);
             })
-            .fail(function(error) {
+            .fail(function (error) {
                 console.log(error);
             });
 
         // stop the form from submitting the normal way and refreshing the page
         event.preventDefault();
     });
-
-
+});
+$(document).ready(function() {
     $('#register-form').submit(function(event) {
         var payload = {
             name: $('input[name=name]').val(),
@@ -31,54 +31,75 @@ $(document).ready(function() {
             email: $('input[name=email]').val(),
             birthdate: $('input[name=birthdate]').val(),
             phone_number: $('input[name=phone_number]').val(),
-            password: $('input[name=password]').val()
+            password: $('input[name=password]').val(),
+            confirm_password: $('input[name=confirm_password]').val()
         };
 
-        $.ajax({
-            type: 'POST',
-            url: '/register',
-            contentType: 'application/json;charset=utf-8',
-            data: JSON.stringify(payload), // our data object
-            dataType: 'json' // what type of data do we expect back from the server
-        })
+
         let errors = validateRegister(payload);
+        console.log(errors);
         if(Object.keys(errors).length > 0){
             event.preventDefault();
-            if (errors['name']) $('#name_error').text(errors['name']);
+            if (errors["name"]) $('#name_error').text(errors["name"]);
             else $('#name_error').text("");
+            if (errors["nameFormat"]) $('#name_error_format').text(errors["nameFormat"]);
+            else $('#name_error_format').text("");
+            if (errors['username']) $('#username_error').text(errors['username']);
+            else $('#username_error').text("");
+            if (errors['usernameFormat']) $('#name_error_format').text(errors['usernameFormat']);
+            else $('#name_error_format').text("");
+            if (errors['password']) $('#password_error').text(errors['password']);
+            else $('#password_error').text("");
+            if (errors['email']) $('#email_error_format').text(errors['email']);
+            else $('#email_error_format').text("");
+            if (errors['phone_number']) $('#phone_number_error').text(errors['phone_number']);
+            else $('#phone_number_error').text("");
+            if (errors['phone_number_length']) $('#phone_number_error_length').text(errors['phone_number_length']);
+            else $('#phone_number_error_length').text("");
+            if (errors["confirm_password"]) $('#confirm_password_error').text(errors["confirm_password"]);
+            else $('#confirm_password_error').text("");
         }
+
     });
 });
 
 function validateRegister(payload) {
-    let errors = [];
+    var errors = [];
 
-    if (payload['name'] === null){
-        errors['name'] = 'The name cannot be empty';
+    if (!payload["name"]){
+        errors["name"] = 'The name cannot be empty';
     }
 
-    /*if (!ctype_alnum(payload['name'] )){
-        errors['nameFormat'] = 'The name must contain only alphanumerical characters';
-    }*/
+    if (!payload["name"].match(/^[a-zA-Z ]+$/) ){
+        errors["nameFormat"] = 'The name must contain only alphanumerical characters';
+    }
 
-    if (payload['username'] === null || payload['username'].length > 20) {
+    if (!payload['username'] || payload['username'].length > 20) {
         errors['username'] = 'The username must be between 1 and 20 characters';
     }
 
-    /*if (!ctype_alnum(payload['username'] )){
+    if (!payload['username'].match(/^[a-zA-Z ]+$/) ){
         errors['usernameFormat'] = 'The username must contain only alphanumerical characters';
-    }*/
+    }
 
-    if (payload['password']  === null || payload['password'].length < 6) {
+    if (!payload['password'] || payload['password'].length < 6) {
         errors['password'] = 'The password must contain at least 6 characters';
     }
 
-    /*if (false === filter_var(payload['email'], FILTER_VALIDATE_EMAIL)) {
+    if ((!/^\w+([\.-]?w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(payload['email']))) {
         errors['email'] = 'The email is not valid';
-    }*/
+    }
 
-    if (payload['confirm_password'] === payload['password']) {
-        errors['repPassword'] = "Password confirmation doesn't match password";
+    if(!payload['phone_number']){
+        errors['phone_number'] = 'Phone number cannot be empty';
+    }
+
+    if(payload['phone_number'].match(/^\d{10}$/)){
+        errors['phone_number_length'] = 'Phone number must be in format XXX XXX XXX';
+    }
+
+    if (payload['confirm_password'] !== payload['password']) {
+        errors["confirm_Password"] = "Password confirmation doesn't match password";
     }
 
 
